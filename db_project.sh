86f479e12f08959b7449cc77c5806fb997f03e5e
omar 3ain <omar3ain@gmail.com>
2022-12-10 23:34:17 +0200

    Table page created and create table function

--- db_project.sh
+++ db_project.sh
@@ -14,8 +14,14 @@ function divider
 {
     echo -e "${NEXTLINE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${NEXTLINE}"
 }
+function invalid_list_input_handle
+{
+    echo -e "${ERRORCOLOR}Please choose from the list${ENDCOLOR}"
+    echo press any key
+    read
+}
 # First_page
-function title 
+function Main_page_title 
 {
     divider;
     echo -e "${TAB}${LIGHTGREY}${BLACK}Bash shell Script project - DBMS Clone${ENDCOLOR}"
@@ -25,7 +31,8 @@ function title
 }
 function First_page
 {
-    title;
+    Main_page_title;
+    echo `pwd`
     select choice in "Create or enter database" "Exit"
     do
     case $REPLY in
@@ -45,18 +52,25 @@ function First_page
             exit
             ;;
         *)
-            echo -e "${ERRORCOLOR}Please choose from the list${ENDCOLOR}"
-            echo press any key
-            read
+            invalid_list_input_handle;
             ;;
     esac
     break
     done
 }
-
+# database_page
+function database_title
+{
+    divider;
+    echo -e "${NEXTLINE}"
+    echo -e "${TAB}${TAB}${TAB}${LIGHTGREY}${BLACK}Welcome to database page${ENDCOLOR}"
+    echo -e "${NEXTLINE}"
+    divider;
+}
 function database_page
 {
-    select choice in "Create Database" "List Databases" "Connect To Databases" "Drop Database"
+    database_title;
+    select choice in "Create Database" "List Databases" "Connect To a Database" "Drop Database" "Back"
     do
     case $REPLY in
         1) 
@@ -65,38 +79,138 @@ function database_page
         2) 
             exit
             ;;
+        3)
+        ;;
+        4)
+        ;;
+        5)
+        cd ..
+        First_page=true
+        database_page=false
+        create_table_page=false
+        ;;
         *)
-            echo -e "${ERRORCOLOR}Please choose from the list${ENDCOLOR}"
-            echo press any key
-            read
+            invalid_list_input_handle;
             ;;
     esac
     break
     done
 }
-function createDB {
-
-    echo -e "Enter The name of database please:"
-    read dbName
-    if [[ $dbName = "" ]]
-    then
-    echo -e "${ERRORCOLOR}Please type the name of database${ENDCOLOR}"
-    echo "Waiting for you database name"
-    read
-    fi [[ ]]
-
-
+function createDB 
+{
+    echo enter the name of the database please
+	read dbName
+	if [[ $dbName = "" ]]
+        then
+            echo -e "${ERRORCOLOR}Cant create a database without a name${ENDCOLOR}"
+            echo Please enter a name 
+            read	
+	elif [[ -e $dbName ]] 
+        then
+            echo -e "${ERRORCOLOR}This database name is already exsists${ENDCOLOR}"
+            echo Please enter another name
+            read
+	elif [[ $dbName =~ ^[a-zA-Z] ]] 
+        then
+            mkdir -p "$dbName"
+            cd "./$dbName" > /dev/null 2>&1
+            echo -e "${BABYBLUE}database created sucessfully${ENDCOLOR}"
+            database_page=false
+            create_table_page=true
+            echo press any key
+            read
+	else
+		echo -e "${ERRORCOLOR}Database name can't start with numbers or special characters${ENDCOLOR}"
+		echo Please try again
+		read
+	fi
+}
+# Table_page
+function table_page_title
+{
+    divider;
+    echo -e "${NEXTLINE}"
+    echo -e "${TAB}${TAB}${LIGHTGREY}${BLACK}Welcome to tables page of database '${dbName}' ${ENDCOLOR}"
+    echo -e "${NEXTLINE}"
+    divider;
+} 
+function table_page 
+{
+    table_page_title;
+    select choice in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table"
+    do
+    case $REPLY in
+        1)
+            create_table;
+            ;;
+        2)
+            list_tables;
+            ;;
+        3)
+            drop_table;
+            ;;
+        4)
+            insert_into_table;
+            ;;
+        5)
+            select_from_table;
+            ;;
+        6)
+            delete_from_table;
+            ;;
+        7)
+            update_table;
+            ;;
+        *)
+            invalid_list_input_handle;
+            ;;
+    esac
+    break
+    done
+}
+function create_table
+{
+    echo -e "Enter Table name please"
+    read table_name
+	if [[ $table_name = "" ]]
+        then
+            echo -e "${ERRORCOLOR}Cant create a table without a name${ENDCOLOR}"
+            echo Please enter a name 
+            read	
+	elif [[ -e $table_name ]] 
+        then
+            echo -e "${ERRORCOLOR}This table name is already exsists${ENDCOLOR}"
+            echo Please enter another name
+            read
+    elif [[ $table_name =~ [/] ]] 
+        then
+            echo -e "${ERRORCOLOR}Can't use / in naming the table${ENDCOLOR}"
+            echo Please enter another name
+            read
+	elif [[ $table_name =~ ^[a-zA-Z] ]] 
+        then
+            touch $table_name
+            #table_fields;
+            echo -e "${BABYBLUE}Table created sucessfully${ENDCOLOR}"
+            echo press any key
+            read
+	fi
 }
 
 while true; do
 	while $First_page;
     do	
-		clear
+		clear;
 		First_page;
 	done
     while $database_page;
     do
-        clear
+        clear;
         database_page;
+    done
+    while $create_table_page;
+    do
+        clear;
+        table_page;
     done
 done
